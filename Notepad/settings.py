44a18d209f8b5174c6067400113fb6e5faff5c4e
007hakan <hakan_yildirim28@hotmail.com>
2020-01-04 15:18:07 +0100

    sfsff

--- Notepad/settings.py
+++ Notepad/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "Notepad/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
