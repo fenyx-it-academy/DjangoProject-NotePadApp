10cd2170c76603b70ec074a57a4797e23b1e926b
DevOps <htunctepe>
2020-01-04 19:12:56 +0100

    nologin islevselligi eklendi

--- article/views.py
+++ article/views.py
@@ -123,10 +123,11 @@ def nologin(request):
     
     if form.is_valid():
         title = form.cleaned_data['title']
-        content =form.cleaned_data['content'] 
-        article_image = form.cleaned_data['article_image']              
-        args ={"title":title, "content":content, "form": form,"article_image":article_image}                
-        return render(request,"index.html", {"args":args})
+        content =form.cleaned_data['content']    
+        dongu_sayisi = [1]           
+        args ={"title":title, "content":content, "form": form, "dongu_sayisi":dongu_sayisi}   
+    
+        return render(request,"nologin.html", {"args":args})
         # return redirect("/", {"args":args})
     
     # return render(request,"index.html",{"form": form})
