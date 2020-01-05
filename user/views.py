38219a76246c0053bd579a2ef68a7b9b2e8e057d
sigma21 <semihkocakel@hotmail.com>
2020-01-05 01:00:14 +0100

    update

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, render_to_response
+from django.shortcuts import render, redirect
 from .forms import RegisterForm, LoginForm, EditProfileForm
 from django.contrib import messages
 from django.contrib.auth.models import User
